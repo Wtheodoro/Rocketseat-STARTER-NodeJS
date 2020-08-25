2ac9b14ef315105a593d760bfa1757c91a2899aa
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-08-24 22:28:00 -0300

    more study

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+// Initializing app
 const app = express()
 
+// Initializing DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+    )
+
+// First rout
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
