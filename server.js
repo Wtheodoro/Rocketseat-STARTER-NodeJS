d4017b06a12bfbb9d4eb6136234a39d8fddd506f
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-08-25 22:31:25 -0300

    up.
    está aconrtecendo algum erro ao  rodar o npm run dev.

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Initializing app
 const app = express()
 
 // Initializing DB
 mongoose.connect(
-    'mongodb://localhost:27017/nodeapi',
-    { useNewUrlParser: true },
-    )
+    "mongodb://localhost:27017/nodeapi"
+)
+requireDir('./src/models')
 
 // First rout
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get("/", (req, res) => {
+    res.send("Hello World")
 })
 
 app.listen(3002)
