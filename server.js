69868df78c1114f2943cbb0b10a9672efaf6c70c
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-08-28 22:49:04 -0300

    up.

--- server.js
+++ server.js
@@ -7,13 +7,21 @@ const app = express()
 
 // Initializing DB
 mongoose.connect(
-    "mongodb://localhost:27017/nodeapi"
+    "mongodb://localhost:27017/nodeapi",
+    { useNewUrlParser: true, useUnifiedTopology: true },
 )
 requireDir('./src/models')
 
+const product = mongoose.model('Product')
+
 // First rout
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    product.create({
+        title: 'React Native',
+        description: 'Build native apps with react',
+        url: 'http://github.com/facebook/react-native'
+    })
+    res.send("Hello World!!!!!")
 })
 
 app.listen(3002)
